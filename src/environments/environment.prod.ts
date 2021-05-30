93271afb0e6361d09c8f9724fa393e19969c5c24
Carlos Javier Bazan Huaman <pacoyx@hotmail.com>
2021-05-30 16:34:21 -0500

    actualizando

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,4 @@
 export const environment = {
   production: true,
-  firebase: {
-    apiKey: "AIzaSyA05ZTVzhjchlchtUEU_E7zgtR3WKvMNWI",
-    authDomain: "ng-fitness-tracker-1003d.firebaseapp.com",
-    projectId: "ng-fitness-tracker-1003d",
-    storageBucket: "ng-fitness-tracker-1003d.appspot.com",
-    messagingSenderId: "25372755953",
-    appId: "1:25372755953:web:804d430e66533df4f2e650"
-  }
+
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,14 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 export const environment = {
   production: false,
-  firebase: {
-    apiKey: "AIzaSyA05ZTVzhjchlchtUEU_E7zgtR3WKvMNWI",
-    authDomain: "ng-fitness-tracker-1003d.firebaseapp.com",
-    projectId: "ng-fitness-tracker-1003d",
-    storageBucket: "ng-fitness-tracker-1003d.appspot.com",
-    messagingSenderId: "25372755953",
-    appId: "1:25372755953:web:804d430e66533df4f2e650"
-  }
+
 };
 
 /*
